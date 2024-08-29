b9f735d0eca1859ddf7210a323c7a6021bc976a4
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-08-30 00:45:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function firstChar(text) {
   // your code here
+	const textTrim = text.trim();
+
+	return textTrim.length > 0 ? textTrim[0] : '';
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
